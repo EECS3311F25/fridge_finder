a62ade60e90b1d2dedef2640a942345f27d68968
nachodoria <ignaciodoriaoberman@gmail.com>
2025-10-27 13:48:59 -0400

    Created Fridge class + added standup folders for organization purposes

---
+++ lib/models/fridge.dart
@@ -0,0 +1,89 @@
+import 'dart:async';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'user.dart';
+import 'item.dart';
+
+class Fridge {
+  final int? id;
+  final User user;
+  final List<Item> items;
+
+  Fridge({
+    this.id,
+    required this.user,
+    required this.items,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'userId': user.id,
+    };
+  }
+
+  factory Fridge.fromMap(Map<String, dynamic> map) {
+    return Fridge(
+      id: map['id'],
+      user: User(
+        id: map['userId'],
+        username: map['username'] ?? 'Unknown',
+        email: map['email'] ?? 'Unknown',
+      ),
+      items: const [], 
+    );
+  }
+}
+
+class FridgeDatabaseHelper {
+  /*static final FridgeDatabaseHelper instance = FridgeDatabaseHelper._instance();
+  static Database? _database;
+
+  FridgeDatabaseHelper._instance();
+
+  Future<Database> get db async {
+    _database ??= await initDb();
+    return _database!;
+  }
+
+  Future<Database> initDb() async {
+    String databasesPath = await getDatabasesPath();
+    String path = join(databasesPath, 'fridge.db');
+
+    return await openDatabase(path, version: 1, onCreate: _onCreate);
+  }
+
+  Future _onCreate(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE fridge (
+        id INTEGER PRIMARY KEY,
+        userId INTEGER
+      )
+    ''');
+  }
+
+  Future<int> insertFridge(Fridge fridge) async {
+    Database db = await instance.db;
+    return await db.insert('fridge', fridge.toMap());
+  }
+
+  Future<List<Map<String, dynamic>>> queryAllFridges() async {
+    Database db = await instance.db;
+    return await db.query('fridge');
+  }
+
+  Future<int> updateFridge(Fridge fridge) async {
+    Database db = await instance.db;
+    return await db.update(
+      'fridge',
+      fridge.toMap(),
+      where: 'id = ?',
+      whereArgs: [fridge.id],
+    );
+  }
+
+  Future<int> deleteFridge(int id) async {
+    Database db = await instance.db;
+    return await db.delete('fridge', where: 'id = ?', whereArgs: [id]);
+  } */
+}
