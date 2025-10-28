4a7722869efbd60a1ab22a96fe50d363dc16f103
Darren <darrenawf@gmail.com>
2025-10-27 20:10:41 -0400

    update to main.dart
    learning how how to create widgets
    added background color, text, styled the text, and aligned the container

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,39 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        backgroundColor: Color.fromRGBO(222, 222, 222, 1),
+        body: Align(
+          alignment: Alignment.topCenter,
+          child: Container(
+            color: Colors.white,
+            child: Column(
+              //Wrap at minimum size (stop at bottom of search)
+              mainAxisSize: MainAxisSize.min,
+              //Children contains Title 'Fridge Finder' and 'Search'
+              children: [
+                // Title for main menu
+                Text(
+                  'Fridge Finder',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 50,
+                  ),
+                ),
+                // Search Text
+                Text(
+                  'Search',
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 30, 30, 30),
+                    fontWeight: FontWeight.bold,
+                    fontSize: 40,
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
