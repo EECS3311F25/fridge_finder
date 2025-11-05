ecc93051aeec139be4f3fb6b332a6b76e2d973a6
shamwoah <noahskinner5@gmail.com>
2025-11-05 12:52:45 -0500

    renamed some documentation files, removed rtd files

--- doc/conf.py
+++
@@ -1,9 +0,0 @@
-project = "Fridge Finder"
-
-latex_engine = "xelatex"
-
-autosectionlabel_prefix_document = True
-
-html_theme = "sphinx_rtd_theme"
-
-html_extra_path = ["static"]
--- doc/test.rst
+++
@@ -1,9 +0,0 @@
-test
-====
-
-here is the Sphinx configuration used to make these docs:
-
-.. literalinclude:: conf.py
-    :language: python
-    :caption: conf.py
-    :linenos:
